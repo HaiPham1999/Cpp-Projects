54ae4464b8a1d4df7906b3da32709eb34eff2646
Hai Pham <haip8513@gmail.com>
2021-09-23 23:37:54 +0700

    Add some change

--- Project01/Login_and_Registration_System.cpp
+++ Project01/Login_and_Registration_System.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 string usr, pwd;
@@ -9,10 +10,10 @@ bool login(void) {
   string name, pass;
   fstream fd;
 
-  std::cout << "Enter your username: ";
-  std::cin >> usr;
-  std::cout << "Enter your password: ";
-  std::cin >> pwd;
+  cout << "Enter your username: ";
+  cin >> usr;
+  cout << "Enter your password: ";
+  cin >> pwd;
 
   fd.open("Info_" + usr + ".txt", ios::in);
   getline(fd, name);
@@ -29,26 +30,24 @@ int main(int argc, char const *argv[]) {
   int choice;
   fstream file;
 
-  std::cout << "LOGIN AND REGISTRATION SYSTEM" << endl
-            << "1. Login" << endl
-            << "2. Registration" << endl;
-  // std::cout << "1. Login" << endl;
-  // std::cout << "2. Registration" << endl;
-  std::cout << "Enter your choice: ";
-  std::cin >> choice;
+  cout << "LOGIN AND REGISTRATION SYSTEM" << endl
+       << "1. Login" << endl
+       << "2. Registration" << endl;
+  cout << "Enter your choice: ";
+  cin >> choice;
 
   if (choice == 1) {
     if (login())
-      std::cout << "Login successfully!" << endl;
+      cout << "Login successfully!" << endl;
     else
-      std::cout << "Login Failed!" << endl;
+      cout << "Login Failed!" << endl;
   }
 
   if (choice == 2) {
-    std::cout << "Enter username: ";
-    std::cin >> usr;
-    std::cout << "Enter password: ";
-    std::cin >> pwd;
+    cout << "Enter username: ";
+    cin >> usr;
+    cout << "Enter password: ";
+    cin >> pwd;
 
     file.open("Info_" + usr + ".txt", ios::out | ios::app);
     file << usr << endl << pwd << endl;
